205a1ea43136fa6566336401a7f34f7e835ebfc3
Frankblation <124920027+Frankblation@users.noreply.github.com>
2025-02-06 02:41:17 -0600

    lets see if vercel agrees

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -1,6 +1,7 @@
-import React from "react";
+export default async function Page({ params }: { params: { id: string } }) {
+  const { id } = await params;
 
-export default async   function Page({ params }: { params: { id: string } }) {
-    await new Promise((resolve) => setTimeout(resolve, 300));
-    return <div>Topics Page: (id)</div>;
+  await new Promise((resolve) => setTimeout(resolve, 300));
+
+  return <div>Topics Page: {id}</div>;
 }
