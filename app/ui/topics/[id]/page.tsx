ce4b82e04c1b10d02d0b21687a2ab89097543409
Frankblation <124920027+Frankblation@users.noreply.github.com>
2025-02-06 02:30:42 -0600

    it's just gonna say id until i fix it

--- app/ui/topics/[id]/page.tsx
+++ app/ui/topics/[id]/page.tsx
@@ -2,5 +2,5 @@ import React from "react";
 
 export default async   function Page({ params }: { params: { id: string } }) {
     await new Promise((resolve) => setTimeout(resolve, 300));
-    return <div>Topics Page: {params.id}</div>;
+    return <div>Topics Page: (id)</div>;
 }
